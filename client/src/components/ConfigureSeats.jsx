909b123aafbcd1f57a9b7f6d741302e62d1ab16b
leesuehee <sueheelee@outlook.com>
2018-06-26 21:52:12 -0700

    ConfigureSeats refactor back to stateless, implementation of lock2 toggles

--- client/src/components/ConfigureSeats.jsx
+++ client/src/components/ConfigureSeats.jsx
@@ -1,41 +1,34 @@
 import React from 'react';
 import SeatArrangement from './SeatArrangement.jsx';
 
-class ConfigureSeats extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lock  : false
-    }
-  }
+let ConfigureSeats = (props) => {
+  let tables = props.state.tables;
 
-  componentDidMount() {
-    console.log(this.props);
-  }
+  return (!props.state.lock2)? (
+    <div className = 'table-configuration'> 
+      Table Capacity: {props.state.tableCapacity}
 
-  render() {
-    let tables = this.props.state.tables;
-
-    return (
-      <div className = 'table-configuration'> 
-        Table Capacity: {this.props.state.tableCapacity}
-  
-        <div className = 'table-map'>
-          {tables.map(table => 
-            <div className='table' key={table.tableId}>
-              {table.tableId}
-              <input type='number' name='seatCapacity'/>
-            </div> 
-          )}
-        </div> 
-        <button onClick={this.props.lock2}> LOCK-IN ARRANGEMENT </button>
-        
-        <div className = 'table-locked'>
-          <SeatArrangement/>
-        </div>
+      <div className = 'table-map'>
+        {tables.map(table => 
+          <div className='table' key={table.tableId}>
+            {table.tableId}
+            <input type='number' name='seatCapacity'/>
+          </div> 
+        )}
+      </div> 
+      <button onClick={props.lock2}> LOCK-IN ARRANGEMENT </button>
+      
+      <div className = 'table-locked'>
+        <SeatArrangement/>
       </div>
+    </div>
     )//end return       
-  }
+      : 
+    (
+      <div>
+        unlocked
+      </div> 
+    ) 
 };
 
 export default ConfigureSeats;
